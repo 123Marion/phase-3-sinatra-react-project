d6dae7c0309c5fd1916daa73d829796373c9310a
@123Marion <ngochocharity@gmail.com>
2022-09-15 20:29:33 +0300

    adds routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,60 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #users
+  get "/users" do
+    users=User.all
+    users.to_json
+  end
+  post "/users" do
+    users= User.create(
+      name: params[:name],
+    )
+    users.to_json
+  end
+
+  #quotes
+  get "/quotes" do
+    quotes=Quote.all
+    quotes.to_json
+  end
+
+  post "/quotes" do
+    quotes = Quote.create(
+      content: params[:content],
+      author: params[:author],
+    )
+    quotes.to_json
+  end
+
+
+
+  #comments
+  get "/comments" do
+    comments= Comment.all
+    comments.to_json
+   end
+ 
+    post "/comments" do
+      comments = Comment.create(
+       comment: params[:comment],
+       like: params[:like]
+     )
+     comments.to_json
+   end
+ 
+   patch "/comments/:id" do
+     comment= Comment.find(params[:id])
+     comment.update(
+       body: params[:body]
+     )
+     comment.to_json
+   end
+ 
+   delete "/comments/:id" do
+     comment = Comment.find(params[:id])
+     comment.destroy
+     comment.to_json
+   end
+
 end
